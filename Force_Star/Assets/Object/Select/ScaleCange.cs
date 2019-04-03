abfca5dcdd51b930e125257bf1b1ca85c2a9d398
NagaoMasaki <karamiso499@gmail.com>
2019-04-03 11:59:21 +0900

    SclectScene修正

--- Force_Star/Assets/Object/Select/ScaleCange.cs
+++ Force_Star/Assets/Object/Select/ScaleCange.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ScaleCange : MonoBehaviour
 {
     [SerializeField]
@@ -9,6 +10,12 @@ public class ScaleCange : MonoBehaviour
 
     [SerializeField]
     float pos_X = -6.0f;
+
+    //最大値
+    public float MAX = 6.0f;
+    //最小値
+    public float MIN = -6.0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +26,18 @@ public class ScaleCange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ParenthesisMove();
+
+
+        //大きさの変更
+        this.transform.localScale = new Vector3(1.5f + (Mathf.Sin(Time.frameCount * s)/4), 1.5f + (Mathf.Sin(Time.frameCount * s) / 4), 1);
+        //位置の移動
+        transform.position = new Vector3(pos_X, transform.position.y, transform.position.z);
+    }
+
+    void ParenthesisMove()
+    {
+        //カッコの移動
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             pos_X += 6.0f;
@@ -29,20 +48,14 @@ public class ScaleCange : MonoBehaviour
             pos_X += -6.0f;
         }
 
-        if(pos_X < -6.0f)
+        if (pos_X < MIN)
         {
-            pos_X = 6.0f;
+            pos_X = MAX;
         }
 
-        if (pos_X > 6.0f)
+        if (pos_X > MAX)
         {
-            pos_X = -6.0f;
+            pos_X = MIN;
         }
-
-
-        //大きさの変更
-        this.transform.localScale = new Vector3(1.5f + (Mathf.Sin(Time.frameCount * s)/4), 1.5f + (Mathf.Sin(Time.frameCount * s) / 4), 1);
-        //位置の移動
-        transform.position = new Vector3(pos_X, transform.position.y, transform.position.z);
     }
 }
