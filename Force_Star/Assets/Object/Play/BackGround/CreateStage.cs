cc1252b4dbf37c7e325583aae09f405738614344
hibikiyoshiyasu <gameword.hibiki@gmail.com>
2019-04-04 15:14:32 +0900

    元に戻した(ヒビキ)

---
+++ Force_Star/Assets/Object/Play/BackGround/CreateStage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateStage : MonoBehaviour
+{
+    //Inspectorに表示される
+    [SerializeField]
+    private int createFloorCount = 12;   // 生成する床の数
+
+    [SerializeField]
+    private GameObject[] valuePrefab = null;  // 生成するプレハブオブジェクト
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < createFloorCount; i++)
+        {
+            Vector2 pos = new Vector2(i * 3, gameObject.transform.position.y);
+            // プレハブからインスタンスを生成
+            GameObject obj = (GameObject)Instantiate(valuePrefab[0], new Vector3(pos.x - 18, pos.y,transform.position.z), Quaternion.identity);
+            // 作成したオブジェクトを子として登録
+            obj.transform.parent = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
