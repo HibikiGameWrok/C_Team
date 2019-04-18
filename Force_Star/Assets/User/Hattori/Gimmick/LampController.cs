bd6c073fc8ea695456daf8ca89f24dc60f7531b7
akihirohattori <hattorisan05@gmail.com>
2019-04-18 11:43:29 +0900

    画像やスクリプトを配置

---
+++ Force_Star/Assets/User/Hattori/Gimmick/LampController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "AttackBoal")
+        {
+
+        }
+    }
+}
