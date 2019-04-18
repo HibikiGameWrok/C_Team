2860a681e1a40e5362ebad7675e3125b00453d15
akihirohattori <hattorisan05@gmail.com>
2019-04-18 13:50:08 +0900

    ランプの処理

--- Force_Star/Assets/User/Hattori/Gimmick/LampController.cs
+++ Force_Star/Assets/User/Hattori/Gimmick/LampController.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class LampController : MonoBehaviour
 {
+    public GameObject starDirec;
+
+    private StarDirector starCreate;
+
     Rigidbody2D rigid2D;
+
+    [SerializeField]
+    private GameObject fire;
+
+    private bool lightFlag = false;
 
     // Start is called before the first frame update
     void Start()
     {
         this.rigid2D = GetComponent<Rigidbody2D>();
+
+        starCreate = starDirec.GetComponent<StarDirector>();
     }
 
     // Update is called once per frame
@@ -22,7 +33,19 @@ public class LampController : MonoBehaviour
     {
         if (col.gameObject.tag == "AttackBoal")
         {
+            if (lightFlag == false)
+            {
+                fire.gameObject.SetActive(true);
 
+                float posX1;
+                float posY;
+                posX1 = this.transform.position.x;
+                posY = this.transform.position.y;
+
+                // 
+                starCreate.CreateOneStar(new Vector2(posX1, posY), 5, false,0.5f);
+                lightFlag = true;
+            }
         }
     }
 }
