94c66d37550e6f0106aebc0dfc7595a04a7b98a6
akihirohattori <hattorisan05@gmail.com>
2019-04-18 16:18:07 +0900

    敵の動きの準備

---
+++ Force_Star/Assets/User/Hattori/TurtleEvent/LeftEnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftEnemyController : MonoBehaviour
+{
+    private float moveSpeed = 0.5f;
+
+    float moveTimer = 0.0f;
+
+    bool moveFlag = true;
+
+    Vector3 keepPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keepPos = this.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveFlag == true)
+        {
+            moveTimer++;
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x - (Mathf.Sin(moveTimer * moveSpeed) * 3.0f), this.transform.localPosition.y, this.transform.localPosition.z);
+        }
+        else
+        {
+            moveTimer = 0;
+            this.transform.localPosition = new Vector3(keepPos.x, this.transform.localPosition.y, this.transform.localPosition.z);
+        }
+
+        if (((Mathf.Sin(moveTimer * moveSpeed) * 3.0f) < -2.8f))
+        {
+            moveFlag = false;
+        }
+    }
+}
