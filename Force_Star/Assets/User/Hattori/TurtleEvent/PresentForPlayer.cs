40837c94f6c24df418cf1aad6916cb145399c985
akihirohattori <hattorisan05@gmail.com>
2019-04-18 14:55:42 +0900

    フェードアウトに苦戦中

--- Force_Star/Assets/User/Hattori/TurtleEvent/PresentForPlayer.cs
+++ Force_Star/Assets/User/Hattori/TurtleEvent/PresentForPlayer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PresentForPlayer : MonoBehaviour
 {
+    SpriteRenderer turtleSprite;
+
     [SerializeField]
     private float shakeSpeed = 0.08f;
 
@@ -12,30 +14,51 @@ public class PresentForPlayer : MonoBehaviour
     [SerializeField]
     private float maxDistance = 0.2f;
 
+    private bool savedFlag = true;
+
+    [SerializeField]
+    private float destroyColor = 255.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        turtleSprite = gameObject.GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //最大値まで行ったら反転
-        if (distance > maxDistance)
+        if (savedFlag != true)
         {
-            shakeSpeed = shakeSpeed * -1;
-            distance = 0.0f;
-        }
-        //最低値まで行ったら反転
-        if (distance < -maxDistance)
-        {
-            shakeSpeed = shakeSpeed * -1;
-            distance = 0.0f;
+            //最大値まで行ったら反転
+            if (distance > maxDistance)
+            {
+                shakeSpeed = shakeSpeed * -1;
+                distance = 0.0f;
+            }
+            //最低値まで行ったら反転
+            if (distance < -maxDistance)
+            {
+                shakeSpeed = shakeSpeed * -1;
+                distance = 0.0f;
+            }
+
+            //敵を移動させる
+            transform.Translate(shakeSpeed, 0, 0);
+            distance += shakeSpeed;
         }
 
-        //敵を移動させる
-        transform.Translate(shakeSpeed, 0, 0);
-        distance += shakeSpeed;
+        if(savedFlag == true)
+        {
+            destroyColor -= 1.0f;
+            turtleSprite.color = new Color(255.0f, 255.0f, 255.0f, destroyColor);
+        }
+
+        if (destroyColor < 0.0f)
+        {
+            destroyColor = 0.0f;
+        }
+
     }
 }
