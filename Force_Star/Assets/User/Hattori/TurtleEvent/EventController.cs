60d82b90b9da881b01ce3ac43ae60c2191a49670
akihirohattori <hattorisan05@gmail.com>
2019-04-22 10:42:46 +0900

    亀の両端の敵が消えたらイベントを完了するという処理を追加

--- Force_Star/Assets/User/Hattori/TurtleEvent/EventController.cs
+++ Force_Star/Assets/User/Hattori/TurtleEvent/EventController.cs
@@ -4,30 +4,44 @@ using UnityEngine;
 
 public class EventController : MonoBehaviour
 {
-    [SerializeField]
     private GameObject turtle;
+    private PresentForPlayer presentForPlayer;
 
-    [SerializeField]
     private GameObject rEnemy;
-
-    [SerializeField]
     private GameObject lEnemy;
+    private LeftEnemyController leftEnemyController;
+    private RightEnemyController rightEnemyController;
+
+
+    private bool[] destroyFlag = new bool[2];
 
     private bool changeFlag = false; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        turtle = GameObject.Find("Turtle");
+        presentForPlayer = turtle.GetComponent<PresentForPlayer>();
+
+        lEnemy = GameObject.Find("lEnemy");
+        rEnemy = GameObject.Find("rEnemy");
+        leftEnemyController = lEnemy.GetComponent<LeftEnemyController>();
+        rightEnemyController = rEnemy.GetComponent<RightEnemyController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(lEnemy.GetComponent<LeftEnemyController>().destroyFlag && rEnemy.GetComponent<RightEnemyController>().destroyFlag)
+        // ゲームオブジェクトが生きている時、敵が生きているか毎フレーム確認
+        destroyFlag[0] = leftEnemyController.destroyFlag;
+        destroyFlag[1] = rightEnemyController.destroyFlag;
+
+        // 両側の敵を倒されていたら
+        if (destroyFlag[0] == true && destroyFlag[1] == true)
         {
             changeFlag = true;
-            turtle.GetComponent<PresentForPlayer>().savedFlag = true;
-        }
+            // 星を出す為の動作
+            presentForPlayer.savedFlag = true;
+        }    
     }
 }
