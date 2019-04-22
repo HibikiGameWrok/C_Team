bfe2a38f79c2acf674260c1c309cc14a5c2f13fb
akihirohattori <hattorisan05@gmail.com>
2019-04-22 12:10:18 +0900

    スクリプトを更新

--- Force_Star/Assets/User/Hattori/Gimmick/BellController.cs
+++ Force_Star/Assets/User/Hattori/Gimmick/BellController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class BellController : MonoBehaviour
 {
+    //public GameObject starDirec;
+
+    //private StarDirector starCreate;
+
+    [SerializeField]
+    private float shakeSize;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //starCreate = starDirec.GetComponent<StarDirector>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "AttackBoal")
+        {
+            transform.Rotate(0.0f, 0.0f, shakeSize);
+        }
     }
 }
