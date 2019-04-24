d629fec8ffedca47335b1e038686c5045d97505a
akihirohattori <hattorisan05@gmail.com>
2019-04-24 20:32:08 +0900

    変更色々

--- Force_Star/Assets/User/Hattori/Gimmick/RandomBottleController.cs
+++ Force_Star/Assets/User/Hattori/Gimmick/RandomBottleController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class RandomBottleController : MonoBehaviour
 {
+    public GameObject[] bottle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //int number = Random.Range(0, bottle.Length);
+        //Instantiate(bottle[number], transform.position, transform.rotation);
     }
 
     // Update is called once per frame
@@ -15,4 +18,6 @@ public class RandomBottleController : MonoBehaviour
     {
         
     }
+
+
 }
