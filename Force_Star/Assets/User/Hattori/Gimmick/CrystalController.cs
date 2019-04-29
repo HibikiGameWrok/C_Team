2a6e044a231badbe29b6130822b08769e9385e1b
akihirohattori <hattorisan05@gmail.com>
2019-04-29 16:17:51 +0900

    スクリプトを作成し処理を書いた

--- Force_Star/Assets/User/Hattori/Gimmick/CrystalController.cs
+++ Force_Star/Assets/User/Hattori/Gimmick/CrystalController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CrystalController : MonoBehaviour
 {
+    public GameObject starDirec;
+
+    private StarDirector starCreate;
+
+    Rigidbody2D rigid2D;
+
+    private int crystalCount = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.rigid2D = GetComponent<Rigidbody2D>();
+
+        starCreate = starDirec.GetComponent<StarDirector>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "AttackBoal")
+        {
+            if (crystalCount < 6)
+            {
+                float posX1;
+                float posY;
+                posX1 = this.transform.position.x;
+                posY = this.transform.position.y;
+
+                starCreate.CreateOneStar(new Vector2(posX1, posY), 5, false, 0.5f);
+                crystalCount += 1;
+            }
+         }
     }
 }
