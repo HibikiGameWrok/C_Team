50400af086d4aaa729d0e843a5cd8257fb5b9b82
hibiki414 <gameword.hibiki@gmail.com>
2019-05-08 11:37:18 +0900

    コード修正 他のステージに移行しない様に修正

--- Force_Star/Assets/Object/Title/Script/SelectFade.cs
+++ Force_Star/Assets/Object/Title/Script/SelectFade.cs
@@ -6,33 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class SelectFade : MonoBehaviour
 {
+    // Imageの赤青緑の値を最大値で固定
+    public const float MAX_RBG = 1;
 
-    public NumbersChange ChangeNum;
     public MoveCameraSelect SelectCamera;
 
     float fadeSpeed = 0.02f;        //透明度が変わるスピードを管理
-    float red, green, blue, alfa;   //パネルの色、不透明度を管理
+    float alfa;   //不透明度を管理
 
     public bool isFadeOut = false;  //フェードアウト処理の開始、完了を管理するフラグ
-    public bool isFadeIn = false;   //フェードイン処理の開始、完了を管理するフラグ
+    public bool isFadeIn = true;   //フェードイン処理の開始、完了を管理するフラグ
 
     Image fadeImage;                //透明度を変更するパネルのイメージ
 
-    [SerializeField]
-    int sceneNum = 0;
-
     bool sceneFlag = false;
-
 
     void Start()
     {
-        sceneNum = ChangeNum.GetStarCounter();
         sceneFlag = SelectCamera.GetChangeFlag();
 
         fadeImage = GetComponent<Image>();
-        red = fadeImage.color.r;
-        green = fadeImage.color.g;
-        blue = fadeImage.color.b;
+
         alfa = fadeImage.color.a;
     }
 
@@ -54,17 +48,16 @@ public class SelectFade : MonoBehaviour
         {
             isFadeOut = true;
         }
-
     }
-
 
 
     void StartFadeIn()
     {
-        alfa -= fadeSpeed;                //a)不透明度を徐々に下げる
+        alfa -= fadeSpeed;               //a)不透明度を徐々に下げる
         SetAlpha();                      //b)変更した不透明度パネルに反映する
         if (alfa <= 0)
-        {                    //c)完全に透明になったら処理を抜ける
+        {                    
+            //c)完全に透明になったら処理を抜ける
             isFadeIn = false;
             fadeImage.enabled = false;    //d)パネルの表示をオフにする
         }
@@ -72,44 +65,26 @@ public class SelectFade : MonoBehaviour
 
     void StartFadeOut()
     {
-        sceneNum = ChangeNum.GetStarCounter();
 
         fadeImage.enabled = true;  // a)パネルの表示をオンにする
         alfa += fadeSpeed;         // b)不透明度を徐々にあげる
         SetAlpha();               // c)変更した透明度をパネルに反映する
         if (alfa >= 1)
-        {             // d)完全に不透明になったら処理を抜ける
+        {            
+            // d)完全に不透明になったら処理を抜ける
             isFadeOut = false;
-
-            //水の星
-            if (sceneNum == 0)
-            {
-                SceneManager.LoadScene("PlayScene");
-            }
-
-            //火の星
-            if (sceneNum == 1)
-            {
-                SceneManager.LoadScene("PlayScene");
-            }
-
-            //機械の星
-            if (sceneNum == 2)
-            {
-                SceneManager.LoadScene("PlayScene");
-            }
-
+            //シーン遷移
+            SceneManager.LoadScene("PlayScene");
         }
     }
 
     void SetAlpha()
     {
-        fadeImage.color = new Color(red, green, blue, alfa);
+        fadeImage.color = new Color(MAX_RBG, MAX_RBG, MAX_RBG, alfa);
     }
 
     public float GetAlfa()
     {
         return alfa;
     }
-
 }
