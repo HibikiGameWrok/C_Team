d19a0bbef52a789ac987504e1835405c55095419
hibiki414 <gameword.hibiki@gmail.com>
2019-05-08 11:45:11 +0900

    SEを鳴らすスクリプトからフラグを検出しBGMの切り替えをする

--- Force_Star/Assets/Object/Title/Script/BgmChange.cs
+++ Force_Star/Assets/Object/Title/Script/BgmChange.cs
@@ -4,34 +4,35 @@ using UnityEngine;
 
 public class BgmChange : MonoBehaviour
 {
-    public NumbersChange number;
+    //
+    public PlaySound PlaySound;
 
     //ステージを選択する前と後のBGM
     public GameObject BGMNormalManager;
     public GameObject BGMHappeningManeger;
 
-
     //BGMの切り替えるflag
-    bool bgmFlag = false; 
+    bool bgmFlag = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        bgmFlag = number.GetStarNumberFlag();
-       
-
+        PlaySound = GetComponent<PlaySound>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        bgmFlag = number.GetStarNumberFlag();
+        bgmFlag = PlaySound.seFlag;
 
         if (bgmFlag)
         {
             //選択する後
             BGMHappeningManeger.SetActive(true);
             BGMNormalManager.SetActive(false);
+
+            // Sceneを遷移してもオブジェクトが消えないようにする
+            DontDestroyOnLoad(this);
         }
         else
         {
