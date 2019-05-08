b468166a6006099662a291f561ae77f3c6f8494e
hibiki414 <gameword.hibiki@gmail.com>
2019-05-09 08:51:08 +0900

    BGMが変わる

--- Force_Star/Assets/Object/Title/Script/BgmChange.cs
+++ Force_Star/Assets/Object/Title/Script/BgmChange.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BgmChange : MonoBehaviour
 {
-    //
-    public PlaySound PlaySound;
+
 
     //ステージを選択する前と後のBGM
     public GameObject BGMNormalManager;
     public GameObject BGMHappeningManeger;
+
+    GameObject pushTitleKey;
+    // SEを鳴らすスクリプト
+    PlaySound playSound;
 
     //BGMの切り替えるflag
     bool bgmFlag = false;
@@ -17,15 +21,17 @@ public class BgmChange : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlaySound = GetComponent<PlaySound>();
+        pushTitleKey = GameObject.Find("PushTitleKey");
+        playSound = pushTitleKey.GetComponent<PlaySound>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        bgmFlag = PlaySound.seFlag;
+        bgmFlag = playSound.seFlag;
+        
+        if (bgmFlag == true || SceneManager.GetActiveScene().name == "PlayScene")
 
-        if (bgmFlag)
         {
             //選択する後
             BGMHappeningManeger.SetActive(true);
@@ -34,7 +40,7 @@ public class BgmChange : MonoBehaviour
             // Sceneを遷移してもオブジェクトが消えないようにする
             DontDestroyOnLoad(this);
         }
-        else
+        else if(bgmFlag == false)
         {
             //選択する前
             BGMHappeningManeger.SetActive(false);
