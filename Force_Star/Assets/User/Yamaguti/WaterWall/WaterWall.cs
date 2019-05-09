71de354461c99bb07ed4b378410a4e395d4d68ab
amezisuto <freedom_liberty48@ezwebne.jp>
2019-05-09 09:27:13 +0900

    コミット

---
+++ Force_Star/Assets/User/Yamaguti/WaterWall/WaterWall.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterWall : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject WaterWallObject=null;
+
+    [SerializeField]
+    private int MaxWater = 1;
+
+    private List<GameObject> myList = new List<GameObject>();
+
+    private GameObject WaterWallClone = null;
+
+    float count;
+
+    Vector3 StartPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position;
+        if (WaterWallObject!=null)
+        {
+            for(int i=0;i<MaxWater;i++)
+            {
+                WaterWallClone = Instantiate(WaterWallObject, StartPosition, Quaternion.identity) as GameObject;
+                WaterWallClone.transform.localScale = transform.localScale;
+                WaterWallClone.transform.position=new Vector3(WaterWallClone.transform.position.x, WaterWallClone.transform.position.y-WaterWallClone.GetComponent<Renderer>().bounds.size.y, WaterWallClone.transform.position.z); 
+                StartPosition = WaterWallClone.transform.position;
+                WaterWallClone.transform.parent = transform;
+                myList.Add(WaterWallClone.gameObject);
+            }
+        }
+        count = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count+=0.1f;
+        if(count>3.0f)
+        {
+            WaterWallClone = Instantiate(WaterWallObject, transform.position, Quaternion.identity) as GameObject;
+            WaterWallClone.transform.localScale = transform.localScale;
+            WaterWallClone.transform.position = new Vector3(WaterWallClone.transform.position.x, WaterWallClone.transform.position.y - WaterWallClone.GetComponent<Renderer>().bounds.size.y, WaterWallClone.transform.position.z);
+            WaterWallClone.transform.parent = transform;
+            myList.Add(WaterWallClone.gameObject);
+            count = 0.0f;
+        }
+    }
+
+    public void WaterList(GameObject Wobject)
+    {
+        Debug.Log(myList.BinarySearch(Wobject));
+        Destroy(Wobject);
+        myList.Remove(Wobject);
+       // Debug.Log(myList[2]);
+    }
+    public void WaterListAt(GameObject Wobject)
+    {
+        int i = myList.IndexOf(Wobject);
+        //for (int i = myList.IndexOf(Wobject); i>0;i--)
+        //{
+        //    Debug.Log(i);
+           // Destroy(myList[i]);
+        //}
+        Debug.Log(i);
+        myList.RemoveAll(myList => myList == Wobject);
+    }
+}
---
+++ Force_Star/Assets/User/Yamaguti/WaterWall/WaterWallMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterWallMove : MonoBehaviour
+{
+    public GameObject Top;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y+-0.1f, transform.position.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ok");
+        if (other.gameObject.tag == "WaterWall")
+        {
+            Top.GetComponent<WaterWall>().WaterList(gameObject);
+            //Destroy(this.gameObject);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            Top.GetComponent<WaterWall>().WaterListAt(gameObject);
+            //Destroy(this.gameObject);
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("ok");
+        if (other.gameObject.tag == "WaterWall")
+        {
+            Top.GetComponent<WaterWall>().WaterList(gameObject);
+           // Destroy(this.gameObject);
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            Top.GetComponent<WaterWall>().WaterListAt(gameObject);
+           // Destroy(this.gameObject);
+        }
+
+    }
+}
