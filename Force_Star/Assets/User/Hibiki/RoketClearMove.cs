74fca925b06be56eb84ec86d08c45381bff8104b
hibiki414 <gameword.hibiki@gmail.com>
2019-05-10 19:53:22 +0900

    クリア時にロケットを修復して飛んでいく処理

---
+++ Force_Star/Assets/User/Hibiki/RoketClearMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoketClearMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        // ロケットに当たっている時
+        if (col.gameObject.tag == "Player")
+        {
+            //    // ↑orWを押すとシーン移行
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            {
+                // コルーチンを実行  
+                StartCoroutine("ClearMove");
+                //SceneManager.LoadScene("ResultScene");
+            }
+        }
+    }
+
+    // コルーチン  
+    private IEnumerator ClearMove()
+    {  
+        // ログ出力  
+        Debug.Log("ロケットに☆を渡した");
+
+        // 1秒待つ  
+        yield return new WaitForSeconds(5.0f);
+
+        Debug.Log("ロケットが修復した");
+
+        // 1秒待つ  
+        yield return new WaitForSeconds(5.0f);
+
+        Debug.Log("ロケットに乗った");
+
+        // 1秒待つ  
+        yield return new WaitForSeconds(5.0f);
+
+        Debug.Log("ロケットが飛んだ");
+
+        // 1秒待つ  
+        yield return new WaitForSeconds(5.0f);
+
+        Debug.Log("リザルトシーンへGO");
+    }
+}
