5938ea85cd0e68da0ef6337dd23af31a7a3b4e48
hibiki414 <gameword.hibiki@gmail.com>
2019-05-14 09:25:01 +0900

    コルーチンを使って演出をする処理を追加。 演出後シーン移行するように追加。

--- Force_Star/Assets/User/Hibiki/RoketClearMove.cs
+++ Force_Star/Assets/User/Hibiki/RoketClearMove.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class RoketClearMove : MonoBehaviour
 {
+    // フェードインするオブジェクト
+    private GameObject Panel = null;
+    // フェードインアウトするscript
+    private StartFade StartFade = null;
+
+    private bool cooltime = false; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // オブジェクトの取得
+        Panel = GameObject.Find("Panel");
+        StartFade = Panel.GetComponent<StartFade>();
     }
 
     // Update is called once per frame
@@ -22,12 +31,17 @@ public class RoketClearMove : MonoBehaviour
         // ロケットに当たっている時
         if (col.gameObject.tag == "Player")
         {
-            //    // ↑orWを押すとシーン移行
-            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            if (cooltime == false)
             {
-                // コルーチンを実行  
-                StartCoroutine("ClearMove");
-                //SceneManager.LoadScene("ResultScene");
+                //    // ↑orWを押すとシーン移行
+                if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+                {
+                    cooltime = true;
+                    StartFade.SetFadeOutFlag(true);
+                    // コルーチンを実行  
+                    StartCoroutine("ClearMove");
+                    //SceneManager.LoadScene("ResultScene");
+                }
             }
         }
     }
@@ -37,24 +51,48 @@ public class RoketClearMove : MonoBehaviour
     {  
         // ログ出力  
         Debug.Log("ロケットに☆を渡した");
+        // 1秒待つ  
+        yield return new WaitForSeconds(5.0f);
+
+        StartFade.SetFadeInFlag(true);
+        
+        //// ☆の破裂プレハブをGameObject型で取得
+        GameObject GoodEff = (GameObject)Resources.Load("GoodEffect");
+        // ☆の破裂プレハブを元に、インスタンスを生成、
+        Instantiate(GoodEff, new Vector3(this.transform.position.x + 1, this.transform.position.y + 3, this.transform.position.z), Quaternion.identity);
+
+        Debug.Log("ロケットの周りに☆が舞う");
 
         // 1秒待つ  
         yield return new WaitForSeconds(5.0f);
 
-        Debug.Log("ロケットが修復した");
+        //// ☆の破裂プレハブをGameObject型で取得
+        GameObject ExStar = (GameObject)Resources.Load("ExplosionStar");
+        // ☆の破裂プレハブを元に、インスタンスを生成、
+        Instantiate(ExStar, new Vector3(this.transform.position.x + 3.8f, this.transform.position.y + 3, this.transform.position.z), Quaternion.identity);
+
+        //// ☆の破裂プレハブをGameObject型で取得
+        GameObject Roket_2 = (GameObject)Resources.Load("Rocket_2");
+        // ☆の破裂プレハブを元に、インスタンスを生成、
+        Instantiate(Roket_2, new Vector3(this.transform.position.x + 3.8f, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+  
+        Debug.Log("ロケットが治った");
 
         // 1秒待つ  
-        yield return new WaitForSeconds(5.0f);
-
-        Debug.Log("ロケットに乗った");
-
-        // 1秒待つ  
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(2.0f);
 
         Debug.Log("ロケットが飛んだ");
 
+        yield return new WaitForSeconds(3.0f);
+
+        Debug.Log("画面暗転");
+        StartFade.SetFadeOutFlag(true);
+
         // 1秒待つ  
         yield return new WaitForSeconds(5.0f);
+
+        //シーン遷移
+        SceneManager.LoadScene("ResultScene");
 
         Debug.Log("リザルトシーンへGO");
     }
