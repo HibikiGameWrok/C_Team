9c7a9be318f9e054740c640f9c11e3bbe7ecff72
hibiki414 <gameword.hibiki@gmail.com>
2019-05-14 12:16:22 +0900

    クリア時の演出scriptを修正

--- Force_Star/Assets/User/Hibiki/RoketClearMove.cs
+++ Force_Star/Assets/User/Hibiki/RoketClearMove.cs
@@ -10,7 +10,8 @@ public class RoketClearMove : MonoBehaviour
     // フェードインアウトするscript
     private StartFade StartFade = null;
 
-    private bool cooltime = false; 
+    // クールタイム
+    bool cooltime = false;
 
     // Start is called before the first frame update
     void Start()
@@ -37,10 +38,10 @@ public class RoketClearMove : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
                 {
                     cooltime = true;
+                    // フェードアウト
                     StartFade.SetFadeOutFlag(true);
                     // コルーチンを実行  
                     StartCoroutine("ClearMove");
-                    //SceneManager.LoadScene("ResultScene");
                 }
             }
         }
@@ -53,7 +54,7 @@ public class RoketClearMove : MonoBehaviour
         Debug.Log("ロケットに☆を渡した");
         // 1秒待つ  
         yield return new WaitForSeconds(5.0f);
-
+        // フェードイン
         StartFade.SetFadeInFlag(true);
         
         //// ☆の破裂プレハブをGameObject型で取得
@@ -66,16 +67,25 @@ public class RoketClearMove : MonoBehaviour
         // 1秒待つ  
         yield return new WaitForSeconds(5.0f);
 
-        //// ☆の破裂プレハブをGameObject型で取得
-        GameObject ExStar = (GameObject)Resources.Load("ExplosionStar");
-        // ☆の破裂プレハブを元に、インスタンスを生成、
-        Instantiate(ExStar, new Vector3(this.transform.position.x + 3.8f, this.transform.position.y + 3, this.transform.position.z), Quaternion.identity);
+        // 自身のスプライトコンポーネントを削除
+        Destroy(GetComponent<SpriteRenderer>());
+
+        // 自身の子オブジェクトを削除
+        foreach (Transform c in this.transform)
+        {
+            GameObject.Destroy(c.gameObject);
+        }
 
         //// ☆の破裂プレハブをGameObject型で取得
+        GameObject ExStar = (GameObject)Resources.Load("ExplosionStar");
+        // ☆の破裂プレハブを元に生成
+        Instantiate(ExStar, new Vector3(this.transform.position.x + 3.8f, this.transform.position.y + 3, this.transform.position.z), Quaternion.identity);
+
+        //// ロケットプレハブをGameObject型で取得
         GameObject Roket_2 = (GameObject)Resources.Load("Rocket_2");
-        // ☆の破裂プレハブを元に、インスタンスを生成、
+        // ロケットプレハブを元に生成
         Instantiate(Roket_2, new Vector3(this.transform.position.x + 3.8f, this.transform.position.y, this.transform.position.z), Quaternion.identity);
-  
+
         Debug.Log("ロケットが治った");
 
         // 1秒待つ  
@@ -86,6 +96,7 @@ public class RoketClearMove : MonoBehaviour
         yield return new WaitForSeconds(3.0f);
 
         Debug.Log("画面暗転");
+        // フェードアウト
         StartFade.SetFadeOutFlag(true);
 
         // 1秒待つ  
