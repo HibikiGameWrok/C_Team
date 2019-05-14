8af77d947e55bb1fc44f00e26f903590bf8e475f
hibiki414 <gameword.hibiki@gmail.com>
2019-05-14 09:17:06 +0900

    パーツを飛ばす処理を追加

---
+++ Force_Star/Assets/Object/Play/Script/PartsMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsMove : MonoBehaviour
+{
+    [SerializeField]
+    private float thrust;
+
+    [SerializeField]
+    private float xthrust;
+
+    [SerializeField]
+    private float ythrust;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector2(this.transform.position.x * thrust + xthrust, this.transform.position.y * thrust + ythrust));
+    }
+}
