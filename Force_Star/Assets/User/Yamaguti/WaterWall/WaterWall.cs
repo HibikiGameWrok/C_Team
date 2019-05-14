d92f8fe293aff2cf8e32bf8d77f9b142c89a2098
amezisuto <freedom_liberty48@ezwebne.jp>
2019-05-14 11:47:32 +0900

    滝動き完成

--- Force_Star/Assets/User/Yamaguti/WaterWall/WaterWall.cs
+++ Force_Star/Assets/User/Yamaguti/WaterWall/WaterWall.cs
@@ -12,9 +12,13 @@ public class WaterWall : MonoBehaviour
 
     private List<GameObject> myList = new List<GameObject>();
 
+    private List<GameObject> sMyList = new List<GameObject>();
+
     private GameObject WaterWallClone = null;
 
     float count;
+
+    bool HitPW = false;
 
     Vector3 StartPosition;
     // Start is called before the first frame update
@@ -54,19 +58,110 @@ public class WaterWall : MonoBehaviour
 
     public void WaterList(GameObject Wobject)
     {
-        int i = myList.IndexOf(Wobject);
-        Destroy(myList[i]);
-        myList.RemoveAt(i);
+        if(myList.IndexOf(Wobject)>-1)
+        {
+            int i = myList.IndexOf(Wobject);
+            Destroy(myList[i]);
+            myList.RemoveAt(i);
+        }
+        else
+        {
+            int i = sMyList.IndexOf(Wobject);
+            Destroy(sMyList[i]);
+            sMyList.RemoveAt(i);
+        }
+
     }
     public void WaterListAt(GameObject Wobject)
     {
 
-        for (int i = 0; i < myList.IndexOf(Wobject)+1; i++)
+        if (myList.IndexOf(Wobject) > -1)
         {
-            Debug.Log(i);
-            Destroy(myList[i]);
+            for (int i = 0; i < myList.IndexOf(Wobject) + 1; i++)
+            {
+                Destroy(myList[i]);
+            }
+            if (sMyList.Count > 0)
+            {
+                for (int i = 0; i < sMyList.Count; i++)
+                {
+                    Debug.Log(sMyList.Count);
+                    Debug.Log(i);
+                    Destroy(sMyList[i]);
+                    Debug.Log(i);
+                }
+                for (int i = 0; i < sMyList.Count; i++)
+                {
+                    Debug.Log(i);
+                    sMyList.RemoveAt(i);
+                }
+                Debug.Log("徹");
+                //   sMyList.RemoveAll(sMyList => sMyList == sMyList);
+                //sMyList.RemoveAt(sMyList.Count);
+            }
+            myList.RemoveAll(myList => myList == Wobject);
+            Debug.Log("徹");
+            
+        }
+        else
+        {
+            for (int i = 0; i < sMyList.IndexOf(Wobject) + 1; i++)
+            {
+                Destroy(sMyList[i]);
+            }
+            //for (int i = 0; i < sMyList.IndexOf(Wobject) + 1; i++)
+            //{
+            //    sMyList.RemoveAt(i);
+            //}
+            sMyList.RemoveAll(sMyList => sMyList == Wobject);
+        }
+        HitPW = true;
+
+    }
+
+    public void SetSList(GameObject obj)
+    {
+        StartPosition = obj.transform.position;
+        for (int i = 0; i < MaxWater; i++)
+        {
+            WaterWallClone = Instantiate(WaterWallObject, StartPosition, Quaternion.identity) as GameObject;
+            WaterWallClone.transform.localScale = transform.localScale;
+            WaterWallClone.transform.position = new Vector3(WaterWallClone.transform.position.x, WaterWallClone.transform.position.y - WaterWallClone.GetComponent<Renderer>().bounds.size.y * (i + 1), WaterWallClone.transform.position.z);
+            // StartPosition.y = StartPosition.y - WaterWallObject.GetComponent<Renderer>().bounds.size.y * (MaxWater - i);
+            WaterWallClone.transform.parent = transform;
+            sMyList.Add(WaterWallClone);
+        }
+        HitPW = false;
+        sMyList.Reverse();
+    }
+    public bool CheckListNum(GameObject obj)
+    {
+        if (myList.IndexOf(obj) > -1)
+        {
+            int i = myList.IndexOf(obj);
+            if (i == 0)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            int i = sMyList.IndexOf(obj);
+            if (i == 0)
+            {
+                return false;
+            }
         }
 
-        myList.RemoveAll(myList => myList == Wobject);
+        //int i = myList.IndexOf(obj);
+        return false;
+    }
+    public bool GetHitCheck()
+    {
+        return HitPW;
+    }
+    public void SetHitflag(bool flag)
+    {
+        HitPW = flag;
     }
 }
