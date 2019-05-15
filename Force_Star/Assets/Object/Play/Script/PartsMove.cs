53e616dd3a5fac1ba6e285297f1a04b95fb92831
hibiki414 <gameword.hibiki@gmail.com>
2019-05-15 16:25:10 +0900

    墜落している時に落ちるパーツを削除する様に修正

--- Force_Star/Assets/Object/Play/Script/PartsMove.cs
+++ Force_Star/Assets/Object/Play/Script/PartsMove.cs
@@ -19,11 +19,20 @@ public class PartsMove : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        // コルーチンを実行  
+        StartCoroutine("Delete");
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.AddForce(new Vector2(this.transform.position.x * thrust + xthrust, this.transform.position.y * thrust + ythrust));
+    }
+
+    private IEnumerator Delete()
+    {
+        // 1秒待つ  
+        yield return new WaitForSeconds(3.0f);
+        Destroy(this.gameObject);
     }
 }
