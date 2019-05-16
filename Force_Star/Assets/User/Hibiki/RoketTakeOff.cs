1fbba1af75b92de450ee536faa584b3e11ec61f1
hibiki414 <gameword.hibiki@gmail.com>
2019-05-16 14:42:10 +0900

    ロケットを飛ぶように修正中、まだできていない

--- Force_Star/Assets/User/Hibiki/RoketTakeOff.cs
+++ Force_Star/Assets/User/Hibiki/RoketTakeOff.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class RoketTakeOff : MonoBehaviour
 {
-    private GameObject mainCamera;
+    private GameObject ParentmainCamera;
     private TargetFollow targetfollow;
 
     // 自身からどれくらい飛ばすか
     [SerializeField]
-    private Vector3 diffPos = new Vector3(0.0f,50.0f,0.0f);
+    private float heightPos = 50.0f;
 
     // 計算座標
-    private Vector3 calcPos;
+    private Vector3 storagePos;
 
     // 速度
     [SerializeField]
@@ -27,19 +27,21 @@ public class RoketTakeOff : MonoBehaviour
     // 動きを止める
     bool stop = false;
 
+    // デバッグ用
+    private Vector3 resetP;
+
     void Awake()
     {
-        mainCamera = GameObject.Find("Main Camera");
-        targetfollow = mainCamera.GetComponent<TargetFollow>();
-
-        // 計算
-        calcPos = this.transform.position + diffPos;
-
         // 開始時間を保管
         startTime = Time.time;
 
-        // ロケットと対象物の距離を保管
-        journeyLength = Vector3.Distance(this.transform.position, calcPos);
+
+        // デバッグ用
+        resetP = this.transform.position;
+
+        // メインカメラを親から取得
+        ParentmainCamera = GameObject.Find("ParentMainCamera");
+        targetfollow = ParentmainCamera.transform.Find("Main Camera").GetComponent<TargetFollow>();
     }
 
     // Update is called once per frame
@@ -47,10 +49,25 @@ public class RoketTakeOff : MonoBehaviour
     {
         // コルーチンを実行  
         StartCoroutine("TakeOff");
+
+        // デバッグ用
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            this.transform.position = resetP;
+            // コルーチンを実行  
+            StartCoroutine("TakeOff");
+        }
+
     }
 
     private IEnumerator TakeOff()
     {
+
+        // 着地地点からの高さを対象物として保管
+        storagePos = new Vector3(this.transform.position.x, heightPos, this.transform.position.z);
+
+        // ロケットと対象物の距離を保管
+        journeyLength = Vector3.Distance(this.transform.position, storagePos);
 
         // 1秒待つ  
         yield return new WaitForSeconds(1.0f);
@@ -62,21 +79,22 @@ public class RoketTakeOff : MonoBehaviour
         // 1秒待つ  
         yield return new WaitForSeconds(3.0f);
         stop = true;
-        // 追従対象を変える
+        // カメラの追従対象を変える
         targetfollow.SetTarget(this.gameObject);
-
+        // 0.1秒待つ  
+        yield return new WaitForSeconds(0.1f);
         // 距離感の速度
-        float distCovered = (Time.time - startTime) * speed;
-
+        float distCovered = (Time.time - startTime);
         // 飛ばす(補間移動)
-        transform.position = Vector3.Lerp(this.transform.position, calcPos, distCovered);
+        this.transform.position = Vector3.Lerp(this.transform.position, storagePos, 3);
+
         //// ロケットプレハブをGameObject型で取得
         //GameObject StartJetPreFab = (GameObject)Resources.Load("StartJetPreFab");
         // ロケットプレハブを元に生成
         //Instantiate(StartJetPreFab, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
-
         // 作成したオブジェクトを子として登録
         //StartJetPreFab.transform.SetParent(this.transform);
+
         StopCoroutine("TakeOff");
     }
 }
