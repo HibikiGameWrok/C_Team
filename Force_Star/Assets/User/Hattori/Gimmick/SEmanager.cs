821dad3ba80d1bb43d1a64256186cbdf4633a1fd
akihirohattori <hattorisan05@gmail.com>
2019-05-16 11:59:12 +0900

    SEの色々

---
+++ Force_Star/Assets/User/Hattori/Gimmick/SEmanager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEmanager : MonoBehaviour
+{
+    private AudioSource sound01;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound01 = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            sound01.PlayOneShot(sound01.clip);
+        }
+    }
+}
