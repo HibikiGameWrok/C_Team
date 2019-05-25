70b5b6edefeb4afd11e02f876e1f268e16106c35
hibikiyoshiyasu <gameword.hibiki@gmail.com>
2019-05-26 03:03:04 +0900

    ・テイクオフのコルーチンでシーン遷移していたのを、ロケットクリアムーブの方でする様に修正。 ・ロケットクリアムーブでプレイヤー、UIを出したり消していた処理をコメント化

--- Force_Star/Assets/User/Hibiki/RoketClearMove.cs
+++ Force_Star/Assets/User/Hibiki/RoketClearMove.cs
@@ -18,16 +18,23 @@ public class RoketClearMove : MonoBehaviour
     // プレイヤー
     private GameObject player = null;
 
-
+    // コントロール
     private PlayerController playercont;
 
+    // プレイヤーのデータ倉庫
     private PlayerDirectorIndex playerIndex;
 
+    // プレイシーンのデータ倉庫
     private PlaySceneDirectorIndex playDrectorIndex;
 
-    private bool oneFlag = false;
+    // 飛ぶロケットオブジェクト
+    private GameObject Roket_sky;
+    // ロケットが飛ぶコルーチンが入ったスクリプト
+    private RoketTakeOff roketTakeOff;
 
-    private bool delUIFlag = false;
+    private bool oneFlag = false;
+    //private bool delUIFlag = false;
+    private bool endFlag = false;
 
     void Awake()
     {
@@ -48,18 +55,37 @@ public class RoketClearMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // コントロール更新
         playercont.Update();
-        // ☆が目標量を達成して尚且つパーツが全て取得できているならば
-        if (playerIndex.GetHaveStarParsent() >= 1 /*&& playerIndex.GetHaveAllPartsFlag()*/)
+
+        // 飛ぶロケットが生成されている時
+        if (Roket_sky != null)
         {
-            if (oneFlag == false)
+            // 毎フレームスクリプト内のフラグ取得関数を変数に保存
+            endFlag = roketTakeOff.GetEndCoruFlag();
+
+            // コルーチンが終わったら
+            if (endFlag == true)
             {
-                GameObject okUI = (GameObject)Resources.Load("paneru");
-                Instantiate(okUI, new Vector3(this.transform.position.x, this.transform.position.y + 10, this.transform.position.z), Quaternion.identity);
-                oneFlag = true;
+                // クリアしたと設定する
+                playDrectorIndex.SetClearFlag();
             }
         }
-    }
+
+
+            // ディレクターが消す様にしてくれる //
+            // ☆が目標量を達成して尚且つパーツが全て取得できているならば
+            //if (playerIndex.GetHaveStarParsent() >= 1 && playerIndex.GetHaveAllPartsFlag())
+            //{
+            //    if (oneFlag == false)
+            //    {
+            //        GameObject okUI = (GameObject)Resources.Load("paneru");
+            //        Instantiate(okUI, new Vector3(this.transform.position.x, this.transform.position.y + 10, this.transform.position.z), Quaternion.identity);
+            //        // 一回だけ生成させるようにする。
+            //        oneFlag = true;
+            //    }
+            //}
+        }
 
     void OnTriggerStay2D(Collider2D col)
     {
@@ -80,7 +106,9 @@ public class RoketClearMove : MonoBehaviour
                         cooltime = true;
                         // フェードアウト
                         StartFade.SetFadeOutFlag(true);
-                        delUIFlag = true;
+
+                        //delUIFlag = true;
+
                         // コルーチンを実行  
                         StartCoroutine("ClearMove");
                     }
@@ -89,14 +117,15 @@ public class RoketClearMove : MonoBehaviour
         }
     }
 
-    public bool GetdelUIFlag()
-    {
-        return delUIFlag;
-    }
+    //public bool GetdelUIFlag()
+    //{
+    //    return delUIFlag;
+    //}
 
-    // コルーチン  
+    // コルーチン ]
+    // 1回だけ呼ばれる為毎フレーム更新されない様になっている 
     private IEnumerator ClearMove()
-    {  
+    {
         // ログ出力  
         Debug.Log("ロケットに☆を渡した");
 
@@ -108,13 +137,15 @@ public class RoketClearMove : MonoBehaviour
             // カメラのターゲットをロケットに変更
             playDrectorIndex.SetObjectTargetCamera(this.gameObject);
         }
+
+        // ディレクターが消してくれる // 
         // プレイヤーを消す
-        player.SetActive(false);
-        player = null;
+        //player.SetActive(false);
+        //player = null;
 
         // フェードイン
         StartFade.SetFadeInFlag(true);
-       
+
         // 1秒待つ  
         yield return new WaitForSeconds(3.0f);
 
@@ -124,6 +155,7 @@ public class RoketClearMove : MonoBehaviour
         // 自身の子オブジェクトを削除
         foreach (Transform c in this.transform)
         {
+            // 子の煙エフェクトを消す
             GameObject.Destroy(c.gameObject);
         }
 
@@ -133,21 +165,19 @@ public class RoketClearMove : MonoBehaviour
         Instantiate(ExStar, new Vector3(this.transform.position.x, this.transform.position.y + 3, this.transform.position.z), Quaternion.identity);
 
         //// ロケットプレハブをGameObject型で取得
-        GameObject Roket_2 = (GameObject)Resources.Load("SkyRoket");
+        Roket_sky = (GameObject)Resources.Load("SkyRoket");
         // ロケットプレハブを元に生成
-        Instantiate(Roket_2, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
+        Instantiate(Roket_sky, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
 
+        // 生成された後に飛ぶロケットの親を取得
+        Roket_sky = GameObject.Find("SkyRoket(Clone)");
+        // スクリプト取得
+        roketTakeOff = Roket_sky.transform.GetChild(0).GetComponent<RoketTakeOff>();
 
         // 5秒待つ  
-        yield return new WaitForSeconds(5.0f);
-
+        yield return new WaitForSeconds(4.0f);
         Debug.Log("ロケットが飛んだ");
-
         // フェードアウト
         StartFade.SetFadeOutFlag(true);
-
-        Debug.Log("画面暗転");
     }
-
-
 }
