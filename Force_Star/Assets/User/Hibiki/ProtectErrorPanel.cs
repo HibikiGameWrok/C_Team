c905a1c3b4fb00f6e312d4bb9e86052c22fc0a8a
hibiki414 <gameword.hibiki@gmail.com>
2019-05-28 17:31:21 +0900

    hierarchyのpanelオブジェクトが非アクティブの状態で起動した時にアクティブにしてエラーが起きないようにした。

---
+++ Force_Star/Assets/User/Hibiki/ProtectErrorPanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtectErrorPanel : MonoBehaviour
+{
+
+    void Start()
+    {
+        if (!this.transform.GetChild(0).gameObject.activeInHierarchy)
+        {
+            Debug.Log(this.transform.GetChild(0).name);
+            this.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
