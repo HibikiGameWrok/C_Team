c22dc73e29fa9ca6c04f0575bcf52c80a3048563
amezisuto <freedom_liberty48@ezwebne.jp>
2019-05-29 13:13:19 +0900

    リザルトのUI位置調整

--- Force_Star/Assets/Object/Result/Script/InsUIOver.cs
+++ Force_Star/Assets/Object/Result/Script/InsUIOver.cs
@@ -26,7 +26,7 @@ public class InsUIOver : MonoBehaviour
         if (stop != true)
         {
             GameObject okUI = (GameObject)Resources.Load("GameOverString");
-            Instantiate(okUI, new Vector3(this.transform.position.x - 40, this.transform.position.y - 1.0f, this.transform.position.z), Quaternion.identity);
+            Instantiate(okUI, new Vector3(this.transform.position.x - 41, this.transform.position.y - 1.0f, this.transform.position.z), Quaternion.identity);
             stop = true;
         }
     }
