4732337291fc47730685e217565afce8102b4056
amezisuto <freedom_liberty48@ezwebne.jp>
2019-05-29 11:00:06 +0900

    パネルを開始時に起きるように修正

--- Force_Star/Assets/User/Hibiki/ProtectErrorPanel.cs
+++ Force_Star/Assets/User/Hibiki/ProtectErrorPanel.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class ProtectErrorPanel : MonoBehaviour
 {
-
-    void Start()
+    void Awake()
     {
         if (!this.transform.GetChild(0).gameObject.activeInHierarchy)
         {
             Debug.Log(this.transform.GetChild(0).name);
             this.transform.GetChild(0).gameObject.SetActive(true);
         }
+    }
+    void Start()
+    {
+
     }
 }
